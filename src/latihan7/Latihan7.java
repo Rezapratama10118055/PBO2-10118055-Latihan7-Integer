1c62e833a0f906e5be09a7be99263aff391cbd46
User <User@DESKTOP-2RC2UP5>
2019-09-25 08:45:25 +0700

    first commit

---
+++ src/latihan7/Latihan7.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan7;
+
+/**
+ *
+ * @author User
+ * NAMA:REZA PRATAMA
+ * KELAS:PBO2
+ * NIM:10118055
+ * Deskripsi:program ini berisi program untuk menampilkan
+ * jenis jenis data bilangan bulat
+ */
+public class Latihan7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long u;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        u = 90000000000L;
+        System.out.println("byte:"+b);
+           System.out.println("short:"+s);
+     System.out.println("int:"+i);
+     System.out.println("long:"+u);
+  
+    }
+    
+}
